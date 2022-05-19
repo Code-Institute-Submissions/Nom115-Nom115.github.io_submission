9de5d06f84e43f271b0daa63d4f2eac4bf5ff9a3
Nom115 <noumair.rafiq@gmail.com>
2022-05-20 00:10:40 +0100

    Add QLED panel logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,6 @@
-console.log("Hello World");
-
+//Moving forwards and backwards for OLED panel
 var oled_counter = 0;
 function oled_page_counter_positive() {
-
-
-   
     oled_counter++;
     console.log(oled_counter)
     if (oled_counter == 1) {
@@ -16,10 +12,8 @@ function oled_page_counter_positive() {
         oled_counter = 2;
     }
 }
-function oled_page_counter_negative() {
 
-
-   
+function oled_page_counter_negative() {   
     oled_counter--;
     console.log(oled_counter)
     if (oled_counter == 0) {
@@ -32,7 +26,7 @@ function oled_page_counter_negative() {
     } else if (oled_counter < 0){
         oled_counter = 0;
     }
-    }
+}
 
 const oled_next = document.getElementById("oled_next");
 oled_next.addEventListener('click', function handleclick(){
@@ -42,4 +36,44 @@ oled_page_counter_positive();
 const oled_prev = document.getElementById("oled_prev");
 oled_prev.addEventListener('click', function handleclick(){
     oled_page_counter_negative();
+    })
+
+//Moving forwards and backwards for QLED panel
+var qled_counter = 0;
+function qled_page_counter_positive() {
+    qled_counter++;
+    console.log(qled_counter)
+    if (qled_counter == 1) {
+        document.getElementById("qled_page").innerHTML = "test";
+        console.log("success")
+    } else if (qled_counter == 2) {
+        document.getElementById("qled_page").innerHTML = "test2";
+    } else if (qled_counter > 2){
+        qled_counter = 2;
+    }
+}
+
+function qled_page_counter_negative() {   
+    qled_counter--;
+    console.log(qled_counter)
+    if (qled_counter == 0) {
+        document.getElementById("qled_page").innerHTML = "The QLED, manufacted by Samsung displays, is known to be the brightest LED backlit panel for consumers. The LED screen powered by Quantum Dots, allow for 64 times as many colors as a standard LED screen, with far better viewing angles. With the high peak brightness, and some Samsung magic, these TV screens compete with OLEDs comfortably.";
+        console.log("success")
+    } else if (qled_counter == 1) {
+        document.getElementById("qled_page").innerHTML = "test";
+    } else if (qled_counter == 2){
+        document.getElementById("qled_page").innerHTML = "test2";
+    } else if (qled_counter < 0){
+        qled_counter = 0;
+    }
+}
+
+const qled_next = document.getElementById("qled_next");
+qled_next.addEventListener('click', function handleclick(){
+qled_page_counter_positive();
+})
+
+const qled_prev = document.getElementById("qled_prev");
+qled_prev.addEventListener('click', function handleclick(){
+    qled_page_counter_negative();
     })
