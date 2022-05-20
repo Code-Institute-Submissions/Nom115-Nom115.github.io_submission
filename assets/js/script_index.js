6f89f9cc88b95f496e73d54efc0d13def6cbe62d
Nom115 <Noumair.rafiq@gmail.com>
2022-05-20 13:50:51 +0100

    Add tv list intial page commit

--- assets/js/script_index.js
+++ assets/js/script_index.js
@@ -4,14 +4,14 @@ var quiz_question = 1;
 function quiz_page_counter_positive() {
     quiz_question++;
     console.log(quiz_question)
-}
+
 //Size
 //Price
 //Windows
 //Blackouts
 //Content watched
 //Wall mounted
-
+}
 
 const quiz_next = document.getElementById("quiz_pos");
 quiz_next.addEventListener('click', function handleclick(){
