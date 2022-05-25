0e076e41243f4e0c030c7a1809e3754b2f5afbea
Nom115 <Noumair.rafiq@gmail.com>
2022-05-25 15:27:00 +0100

    Add Sony TVs to TV list

--- assets/js/script_tv_list.js
+++ assets/js/script_tv_list.js
@@ -13,16 +13,21 @@ var tabledata = [
     {id:10, name:"Samsung", model:"Q60A", price:699, links:"https://www.amazon.co.uk/Samsung-55Q60A-139-7-Ultra-Smart/dp/B092R8XYR9/ref=sr_1_3?crid=168GJHP7F6TJF&keywords=Q60A+55&qid=1653485844&s=electronics&sprefix=q60a+55%2Celectronics%2C64&sr=1-3"},
 
     //LG TVs
-    {id:1, name:"LG", model:"C1 OLED", price:1299, links:"https://www.amazon.co.uk/LG-OLED55C14LB-Advanced-processor-Assistant/dp/B08X3QYNT5/ref=sr_1_1?crid=2VI0A737B1LE4&keywords=LG%2BC1%2B55&qid=1653486946&s=electronics&sprefix=lg%2Bc1%2B55%2Celectronics%2C74&sr=1-1&th=1"},
-    {id:2, name:"LG", model:"B1 OLED", price:999, links:"https://www.amazon.co.uk/LG-OLED55B16LA-processor-SELF-LIT-Assistant/dp/B08X3R8XMC/ref=sr_1_3?crid=1SWI98EJTRVX&keywords=LG%2BB1%2B55&qid=1653486995&s=electronics&sprefix=lg%2Bb%2Celectronics%2C61&sr=1-3&th=1"},
-    {id:3, name:"LG", model:"A1 OLED", price:899, links:"https://www.amazon.co.uk/LG-Inch-OLED-Ultra-Smart/dp/B095P9B15P/ref=sr_1_4?crid=NPTU5G80W3BA&keywords=LG+A1+55&qid=1653487090&s=electronics&sprefix=lg+a%2Celectronics%2C68&sr=1-4"},
-    {id:4, name:"LG", model:"G1 OLED", price:1599, links:"https://www.amazon.co.uk/LG-OLED55G16LA-Ultra-Freeview-Freesat/dp/B0946Y4S59/ref=sr_1_3?crid=133Y7IGIL6W46&keywords=LG+G1+55&qid=1653487113&s=electronics&sprefix=lg+g%2Celectronics%2C66&sr=1-3"},
-    {id:5, name:"LG", model:'Z1 8K OLED (77")', price:19999, links:"https://www.lg.com/uk/tvs/lg-oled77z19la"},
-    {id:6, name:"LG", model:"NANO90", price:849, links:"https://www.amazon.co.uk/LG-55NANO916PA-NanoCell-processor-Assistant/dp/B08X3R2YR6/ref=sr_1_1?crid=2KPOB940HJGH0&keywords=LG%2BNANO90%2B55&qid=1653487309&s=electronics&sprefix=lg%2Bnano90%2B55%2Celectronics%2C66&sr=1-1&th=1"},
-    {id:7, name:"LG", model:"UP81", price:499, links:"https://www.amazon.co.uk/LG-55UP81006LA-Freeview-Assistant-compatible/dp/B08X3PLQDX/ref=sr_1_5?crid=9GOTYIR2TNE6&keywords=LG%2BUP7000%2B55%22&qid=1653487357&s=electronics&sprefix=lg%2Bup7000%2B55%2B%2Celectronics%2C53&sr=1-5&th=1"},
-    {id:8, name:"LG", model:"NANO86", price:699, links:"https://www.amazon.co.uk/55NANO886PB-55-NanoCell-Ultra-Smart/dp/B094KQKD5M/ref=sr_1_2?crid=2TCWRE5FQ73YP&keywords=lg+nano+75+55%22&qid=1653487399&s=electronics&sprefix=lg+nano75%2Celectronics%2C55&sr=1-2"},
+    {id:11, name:"LG", model:"C1 OLED", price:1299, links:"https://www.amazon.co.uk/LG-OLED55C14LB-Advanced-processor-Assistant/dp/B08X3QYNT5/ref=sr_1_1?crid=2VI0A737B1LE4&keywords=LG%2BC1%2B55&qid=1653486946&s=electronics&sprefix=lg%2Bc1%2B55%2Celectronics%2C74&sr=1-1&th=1"},
+    {id:12, name:"LG", model:"B1 OLED", price:999, links:"https://www.amazon.co.uk/LG-OLED55B16LA-processor-SELF-LIT-Assistant/dp/B08X3R8XMC/ref=sr_1_3?crid=1SWI98EJTRVX&keywords=LG%2BB1%2B55&qid=1653486995&s=electronics&sprefix=lg%2Bb%2Celectronics%2C61&sr=1-3&th=1"},
+    {id:13, name:"LG", model:"A1 OLED", price:899, links:"https://www.amazon.co.uk/LG-Inch-OLED-Ultra-Smart/dp/B095P9B15P/ref=sr_1_4?crid=NPTU5G80W3BA&keywords=LG+A1+55&qid=1653487090&s=electronics&sprefix=lg+a%2Celectronics%2C68&sr=1-4"},
+    {id:14, name:"LG", model:"G1 OLED", price:1599, links:"https://www.amazon.co.uk/LG-OLED55G16LA-Ultra-Freeview-Freesat/dp/B0946Y4S59/ref=sr_1_3?crid=133Y7IGIL6W46&keywords=LG+G1+55&qid=1653487113&s=electronics&sprefix=lg+g%2Celectronics%2C66&sr=1-3"},
+    {id:15, name:"LG", model:'Z1 8K OLED (77")', price:19999, links:"https://www.lg.com/uk/tvs/lg-oled77z19la"},
+    {id:16, name:"LG", model:"NANO90", price:849, links:"https://www.amazon.co.uk/LG-55NANO916PA-NanoCell-processor-Assistant/dp/B08X3R2YR6/ref=sr_1_1?crid=2KPOB940HJGH0&keywords=LG%2BNANO90%2B55&qid=1653487309&s=electronics&sprefix=lg%2Bnano90%2B55%2Celectronics%2C66&sr=1-1&th=1"},
+    {id:17, name:"LG", model:"UP81", price:499, links:"https://www.amazon.co.uk/LG-55UP81006LA-Freeview-Assistant-compatible/dp/B08X3PLQDX/ref=sr_1_5?crid=9GOTYIR2TNE6&keywords=LG%2BUP7000%2B55%22&qid=1653487357&s=electronics&sprefix=lg%2Bup7000%2B55%2B%2Celectronics%2C53&sr=1-5&th=1"},
+    {id:18, name:"LG", model:"NANO86", price:699, links:"https://www.amazon.co.uk/55NANO886PB-55-NanoCell-Ultra-Smart/dp/B094KQKD5M/ref=sr_1_2?crid=2TCWRE5FQ73YP&keywords=lg+nano+75+55%22&qid=1653487399&s=electronics&sprefix=lg+nano75%2Celectronics%2C55&sr=1-2"},
     
-
+    //Sony TVs
+    {id:19, name:"Sony", model:"A90J", price:3499, links:"https://www.amazon.co.uk/Sony-BRAVIA-XR-OLED-XR55A90J-Black/dp/B098R7H8GX/ref=sr_1_2?crid=283H7QQB74LLC&keywords=Sony%2BA90J&qid=1653488315&s=electronics&sprefix=sony%2Ba90j%2Celectronics%2C74&sr=1-2&th=1"},
+    {id:20, name:"Sony", model:"A80J", price:2499, links:"https://www.amazon.co.uk/Sony-BRAVIA-XR-OLED-XR55A80J-Black/dp/B097PV9ZZ1/ref=sr_1_1?crid=3L3WZI5IU63A5&keywords=Sony+A80J&qid=1653488399&s=electronics&sprefix=sony+a80j%2Celectronics%2C62&sr=1-1"},
+    {id:21, name:"Sony", model:'X95J (65")', price:1999, links:"https://www.amazon.co.uk/Sony-BRAVIA-Array-XR-65X95J-65-inch-Black/dp/B098R8DPD8/ref=sr_1_2?crid=3MVUU749NIP5S&keywords=Sony+X95J+55&qid=1653488524&s=electronics&sprefix=sony+x95j+55%2Celectronics%2C53&sr=1-2"},
+    {id:22, name:"Sony", model:"X90J", price:1599, links:"https://www.amazon.co.uk/Sony-BRAVIA-Array-Google-Smart-Black/dp/B098R896RX/ref=sr_1_1?crid=3HN4D2R97QEH9&keywords=Sony%2BX90J%2B55&qid=1653488587&s=electronics&sprefix=sony%2Bx90%2Celectronics%2C56&sr=1-1&th=1"},
+    {id:23, name:"Sony", model:"X85J", price:899, links:"https://www.amazon.co.uk/Sony-BRAVIA-KD55X89JU-Freeview-Freesat/dp/B097N65YKX/ref=sr_1_1?crid=1DVVXROWMN2IL&keywords=Sony+X85J+55&qid=1653488762&s=electronics&sprefix=sony+x85%2Celectronics%2C57&sr=1-1"},
     
 ];
 
