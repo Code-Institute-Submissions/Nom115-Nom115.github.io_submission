83ace9cf93dc248c15ec549ded9dc3d877099767
Nom115 <Noumair.rafiq@gmail.com>
2022-06-06 19:26:21 +0100

    Add unhiding to rest of quiz elements upon start click

--- assets/js/script_index.js
+++ assets/js/script_index.js
@@ -1,10 +1,13 @@
 //Define variables
 const startButtonElement = document.getElementById('start_btn')
+const questionContainer = document.getElementById('quiz')
+
 
 //functions
 
 //start button function
 startButtonElement.addEventListener("click", function startQuizEvent() {
     startButtonElement.style.visibility = 'hidden';
+    questionContainer.style.visibility = 'visible';
     console.log("success")
 })
