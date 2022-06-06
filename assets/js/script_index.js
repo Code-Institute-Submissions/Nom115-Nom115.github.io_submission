d67aca6f9703aca052f8588c7d841b79ecabc9bd
Nom115 <Noumair.rafiq@gmail.com>
2022-06-06 19:18:41 +0100

    Add hiding of start button

---
+++ assets/js/script_index.js
@@ -0,0 +1,10 @@
+//Define variables
+const startButtonElement = document.getElementById('start_btn')
+
+//functions
+
+//start button function
+startButtonElement.addEventListener("click", function startQuizEvent() {
+    startButtonElement.style.visibility = 'hidden';
+    console.log("success")
+})
